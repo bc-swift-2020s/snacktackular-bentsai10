92e63ce38d046db7add0ff46095757bbcf524431
Benjamin Tsai <benjamintsa@gmail.com>
2020-04-13 11:22:26 -0700

    Loading data with cloud firestorm (9.6)

--- Snacktacular/Spots.swift
+++ Snacktacular/Spots.swift
@@ -7,7 +7,31 @@
 //
 
 import Foundation
+import Firebase
 
 class Spots{
     var spotsArray = [Spot]()
+    var db: Firestore!
+    
+    init(){
+        db = Firestore.firestore()
+    }
+    
+    func loadData(completed: @escaping () -> ()){
+        db.collection("spots").addSnapshotListener{ (querySnapshot, error) in
+            guard error == nil else{
+                print("Error")
+                return completed()
+            }
+            self.spotsArray = []
+            //there are querySnapshot!.documents.count documents in the spots snapshot
+            for document in querySnapshot!.documents{
+                let spot = Spot(dictionary: document.data())
+                spot.documentID = document.documentID
+                self.spotsArray.append(spot)
+            }
+            completed()
+            
+        }
+    }
 }
