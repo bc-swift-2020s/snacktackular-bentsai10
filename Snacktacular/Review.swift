93a8cde450c869a3ec76930edbaee07566afc535
Benjamin Tsai <benjamintsa@gmail.com>
2020-04-18 19:42:12 -0700

    Loading reviews using listener (9.15) [Error to be fixed later]

--- Snacktacular/Review.swift
+++ Snacktacular/Review.swift
@@ -34,6 +34,17 @@ class Review{
         let currentUserID = Auth.auth().currentUser?.email ?? "Unknown user"
         self.init(title: "", text: "", rating: 0, reviewerUserID: currentUserID, date: Date(), documentID: "")
     }
+    
+    convenience init(dictionary: [String: Any]){
+        let title = dictionary["title"] as! String? ?? ""
+        let text = dictionary["text"] as! String? ?? ""
+        let rating = dictionary["rating"] as! Int? ?? 0
+        let reviewerUserID = dictionary["reviewerUserID"] as! String? ?? ""
+        let date = dictionary["date"] as! Date? ?? Date()
+        self.init(title: title, text: text, rating: rating, reviewerUserID: reviewerUserID, date: date, documentID: "")
+        
+    }
+    
     func saveData(spot:Spot, completed: @escaping (Bool) -> ()) {
         let db = Firestore.firestore()
         //create the dictionary representing the data we want to save
