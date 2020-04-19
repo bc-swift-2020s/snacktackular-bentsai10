0c944dad5bc4192a1b7d9478c62620fd8b5aa759
Benjamin Tsai <benjamintsa@gmail.com>
2020-04-18 18:37:01 -0700

    Working with Outlet collections (9.13)

--- Snacktacular/ReviewTableViewController.swift
+++ Snacktacular/ReviewTableViewController.swift
@@ -19,6 +19,17 @@ class ReviewTableViewController: UITableViewController {
     @IBOutlet weak var saveBarButton: UIBarButtonItem!
     @IBOutlet weak var deleteButton: UIButton!
     @IBOutlet weak var buttonsBackgroundView: UIView!
+    @IBOutlet var starButtonCollection: [UIButton]!
+    
+    var rating = 0{
+        didSet{
+            for starButton in starButtonCollection{
+                let image = UIImage(named: (starButton.tag < rating ? "star-filled": "star-empty"))
+                starButton.setImage(image, for: .normal)
+            }
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         let tap = UITapGestureRecognizer(target: self.view, action: #selector(UIView.endEditing(_:)))
@@ -32,6 +43,9 @@ class ReviewTableViewController: UITableViewController {
         } else {
             navigationController?.popViewController(animated: true)
         }
+    }
+    @IBAction func starButtonPressed(_ sender: UIButton) {
+        rating = sender.tag + 1
     }
     
     @IBAction func reviewTitleChanged(_ sender: UITextField) {
